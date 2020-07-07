3be5d3a4459f143922a98a526a0464231cb4b8e3
Yurj Tugolukov <yurj9999@yandex.ru>
2020-07-07 21:12:56 +0300

    five part-first task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  static instance;
+  element = null;
+
+  constructor(message = '', {
+    duration = 0,
+    type = ''
+  } = {}) {
+
+    NotificationMessage.instance = this;
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  render() {
+
+    const element = document.createElement('div');
+
+    element.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">${this.message}</div>
+        </div>
+      </div>
+    `;
+
+    this.element = element.firstElementChild;
+
+  }
+
+  show(parent = document.body) {
+    parent.append(this.element);
+
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
+
