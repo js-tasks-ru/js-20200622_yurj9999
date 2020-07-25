0c3a546037d5fade1e7bae1dc1a760d6fc00c0e5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-07-25 19:48:35 +0300

    Added task ProductForm компонент с отправкой/загрузкой данных с сервера

---
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -0,0 +1,10 @@
+import SortableList from '../../2-sortable-list/solution/index.js';
+import escapeHtml from './utils/escape-html.js';
+import fetchJson from './utils/fetch-json.js';
+
+const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
+const BACKEND_URL = 'https://course-js.javascript.ru';
+
+export default class ProductForm {
+
+}
