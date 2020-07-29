741d38a6ff0bdc67a7228028c64a3fcda939f97b
Vladimir Shevchuk <dosandkv@gmail.com>
2020-07-29 13:06:41 +0300

    Update jest-setup-files-after-env.js

--- jest-setup-files-after-env.js
+++ jest-setup-files-after-env.js
@@ -4,6 +4,6 @@ import fetchMock from 'jest-fetch-mock';
 fetchMock.enableMocks();
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.log('reason: ', reason, 'promise: ', promise);
+  // console.log('reason: ', reason, 'promise: ', promise);
   /* Заглушка в node для window.addEventListener('unhandledrejection') */
 });
